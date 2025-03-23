f6f2fa98ae5c177fcacfeefd1591f7e2cff2d444
Luiz <luizindsc@proton.me>
2025-03-23 00:09:49 -0300

    Add task input functionality; implement input validation and update button action

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Hello World!");
+function taskInput () {
+    let input = document.getElementById("taskInput").value;
+
+    // check if the input is empty
+
+    if (input.trim() === "") {
+        alert("Please enter a task");
+    }
+    else {
+        let task = document.createElement("li");
+        task.innerHTML = input;
+        document.getElementById("taskList").appendChild
+    }
+}
